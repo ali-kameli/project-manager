915e480154eeafcf70ed823eb400e56cc1d84bd0
alikameli <alikameli0123@gmail.com>
2023-09-03 08:54:20 +0330

    tags for project

--- app/http/controllers/project.controller.js
+++ app/http/controllers/project.controller.js
@@ -3,9 +3,10 @@ const { ProjectModel } = require("../../models/project.model");
 class ProjectController {
     async createProject(req, res, next) {
         try {
-            const { title, text, image } = req.body;
+            const { title, text, image, tags } = req.body;
             const owner = req.user._id;
-            const project = await ProjectModel.create({ title, text, owner, image });
+            console.log(tags);
+            const project = await ProjectModel.create({ title, text, owner, image, tags });
             if (!project) throw { staus: 400, message: "create project has been failed" };
             return res.status(201).json({
                 status: 201,
--- app/http/validations/project.validation.js
+++ app/http/validations/project.validation.js
@@ -3,7 +3,7 @@ const { body } = require('express-validator');
 exports.createProjectValidation = () => {
     return [
         body('title').notEmpty().withMessage('project title must be filled'),
-        body('text').notEmpty().withMessage('project description must be filled')
-        ,
+        body('text').notEmpty().withMessage('project description must be filled'),
+        body('tags').isArray({ min: 0, max: 10 }).withMessage('maximum tags is 10 word'),
     ]
 }
