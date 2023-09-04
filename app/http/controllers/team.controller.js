4eacd0b63734785a489ac374f208a1f17ae493a1
alikameli <alikameli0123@gmail.com>
2023-09-04 12:12:34 +0330

    delete team

--- app/http/controllers/team.controller.js
+++ app/http/controllers/team.controller.js
@@ -1,6 +1,10 @@
+const autoBind = require("auto-bind");
 const { TeamModel } = require("../../models/team.model");
 
 class TeamController {
+    constructor() {
+        autoBind(this);
+    }
     async createTeam(req, res, next) {
         try {
             const { name, username, description } = req.body;
@@ -27,6 +31,59 @@ class TeamController {
             })
         } catch (error) {
             next(error);
+        }
+    }
+    async findTeam(teamID) {
+        const team = await TeamModel.findById(teamID);
+        if (!team) throw { status: 404, message: "team not found" };
+        return team;
+    }
+    async getTeamById(req, res, next) {
+        try {
+            const teamID = req.params.id;
+            const team = await this.findTeam(teamID);
+            return res.status(200).json({
+                status: 200,
+                success: true,
+                team
+            })
+        } catch (error) {
+            next(error)
+        }
+    }
+    async getMyTeams(req, res, next) {
+        try {
+            const userID = req.user._id;
+            const teams = await TeamModel.find({
+                $or: [
+                    { owner: userID },
+                    { users: userID }
+                ]
+            });
+            res.status(200).json({
+                status: 200,
+                success: true,
+                teams
+            })
+        } catch (error) {
+            next(error)
+        }
+    }
+    async removeTeamById(req, res, next) {
+        try {
+            const teamID = req.params.id;
+            const owner = req.user._id;
+            await this.findTeam(teamID);
+            const result = await TeamModel.deleteOne({ _id: teamID, owner });
+            
+            if (result.deletedCount == 0) throw { status: 500, message: "delete team has been failed" }
+            return res.status(200).json({
+                status: 200,
+                success: true,
+                message: "delete projectc successfully"
+            })
+        } catch (error) {
+            next(error)
         }
     }
     inviteUserToTeam() { }
--- app/routes/team.routes.js
+++ app/routes/team.routes.js
@@ -3,10 +3,13 @@ const { TeamController } = require('../http/controllers/team.controller');
 const { checkLogin } = require('../http/middlewares/autoLogin');
 const { creatTeamValidator } = require('../http/validations/team.validation');
 const { expressValidationMapper } = require('../http/middlewares/checkErrors');
+const { mongoIdValidator } = require('../http/validations/public');
 
 router.post('/create', checkLogin, creatTeamValidator(), expressValidationMapper, TeamController.createTeam)
 router.get('/list', checkLogin, TeamController.getListsOfTeam);
-router.get('/:id', checkLogin, TeamController.getTeamById)
+router.get('/me', checkLogin, TeamController.getMyTeams);
+router.get('/:id', checkLogin, mongoIdValidator(), TeamController.getTeamById);
+router.delete('/remove/:id', checkLogin, mongoIdValidator(), expressValidationMapper, TeamController.removeTeamById)
 
 module.exports = {
     teamRoutes: router
